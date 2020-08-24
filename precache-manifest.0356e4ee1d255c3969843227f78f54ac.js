40e542010b924f747b4ec5103b0e585e22decf10
seth-rollins1 <utmset@gmail.com>
2020-08-24 19:19:55 +0530

    Updates

---
+++ precache-manifest.0356e4ee1d255c3969843227f78f54ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93931ec3e33aad173a13c6e1426cb262",
+    "url": "/kaleem-escapes/index.html"
+  },
+  {
+    "revision": "c66461e4982bca2375ad",
+    "url": "/kaleem-escapes/static/css/main.3f99637d.chunk.css"
+  },
+  {
+    "revision": "65b276b7d689efe676ea",
+    "url": "/kaleem-escapes/static/js/2.eaaa55b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kaleem-escapes/static/js/2.eaaa55b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66461e4982bca2375ad",
+    "url": "/kaleem-escapes/static/js/main.3f650673.chunk.js"
+  },
+  {
+    "revision": "42eedef04422dcfe6655",
+    "url": "/kaleem-escapes/static/js/runtime-main.0d6d3206.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kaleem-escapes/static/media/logo.5d5d9eef.svg"
+  }
+]);
